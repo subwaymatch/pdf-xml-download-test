266724b4fc7052adff2cdc2c5e803f1b565bae36
Ye Joo Park <subwaymatch@gmail.com>
2020-08-02 19:15:07 -0500

    Remove unnecessary code

--- pages/api/file/pdf.js
+++ pages/api/file/pdf.js
@@ -2,13 +2,10 @@
 
 import fs from 'fs';
 import path from 'path';
-import getConfig from 'next/config';
-const { serverRuntimeConfig } = getConfig();
 
 export default (req, res) => {
   const pdfContent = fs.readFileSync(
     path.resolve('./public/files/pdf-file.pdf')
-    // path.join(serverRuntimeConfig.PROJECT_ROOT, './files/pdf-file.pdf')
   );
 
   res.setHeader('Content-Type', 'application/pdf');
--- pages/api/file/xml.js
+++ pages/api/file/xml.js
@@ -2,14 +2,9 @@
 
 import fs from 'fs';
 import path from 'path';
-import getConfig from 'next/config';
-const { serverRuntimeConfig } = getConfig();
 
 export default (req, res) => {
-  const xml = fs.readFileSync(
-    path.resolve('./public/files/delivery.xml')
-    // path.join(serverRuntimeConfig.PROJECT_ROOT, './files/delivery.xml')
-  );
+  const xml = fs.readFileSync(path.resolve('./public/files/delivery.xml'));
 
   res.setHeader('Content-Type', 'text/xml');
   res.statusCode = 200;
